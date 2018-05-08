bfca7875b25611799e5e690dc6f038a890542211
Hiroki Kato <hkato@sas.upenn.edu>
2018-05-08 16:33:10 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,11 +14,11 @@ def assign_rooms(attendees)
   binding.pry
   attendees.collect do |attendee|
     room = 0
-  "Hello, #{attendee}! You'll be assigned to room #{room += 1}!"
+  "Hello, #{attendee}! You'll be assigned to room #{room}!"
+    room += 1
+  end
 end
 
-attendees = ["john", "steve"]
-assign_rooms(attendees)
 
 
 
