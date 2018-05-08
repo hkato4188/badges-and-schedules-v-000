95e5698053692faa10a8128d37735c8b27bb8932
Hiroki Kato <hkato@sas.upenn.edu>
2018-05-08 16:50:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,9 +21,9 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-
   batch_badge_creator(attendees).each do |attendee|
     puts "#{attendee}"
+  end
   assign_rooms(attendees).each do |attendee|
     puts "#{attendee}"
   end
