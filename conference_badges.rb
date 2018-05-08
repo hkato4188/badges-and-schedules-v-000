1b20cb734b84a37e02ba6179234a1af48e0223b2
Hiroki Kato <hkato@sas.upenn.edu>
2018-05-08 16:37:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  # binding.pry
+  binding.pry
   attendees.each_with_index do |attendee, room|
     list = Array.new
     list << "Hello, #{attendee}! You'll be assigned to room #{room + 1}!"
